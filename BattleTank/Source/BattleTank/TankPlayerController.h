1fa06611f03ebde888f698c4820386e8c31c0450
Unknown <graushf@gmail.com>
2018-12-22 12:52:22 +0100

    Crosshair position in screen coordinates

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -16,17 +16,24 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 
 public:
-	ATank* GetControlledTank() const;
-
+	
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaTime) override;
 
+private:
+
+	UPROPERTY(EditAnywhere, Category="Crosshair Setup")
+	float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere, Category = "Crosshair Setup")
+	float CrosshairYLocation = 0.3333;
+
+	ATank* GetControlledTank() const;
+
 	// Start the  tank moving the barrel so that a shot would hit where
 	// the crosshair intersects the world
 	void AimTowardsCrosshair();
-
-private:
 	
 	// Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& HitLocation) const;

35517a2d9c21ac3b025e7c8c6b2e6baa81dcf285
Unknown <graushf@gmail.com>
2018-12-22 12:36:00 +0100

    Adding GetSightRayHitLocation out parameter method

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -19,8 +19,7 @@ void ATankPlayerController::BeginPlay()
 void ATankPlayerController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	// AimTowardsCrosshair()
-	UE_LOG(LogTemp, Warning, TEXT("Player controller ticking"));
+	AimTowardsCrosshair();
 }
 
 ATank* ATankPlayerController::GetControlledTank() const 
@@ -32,8 +31,19 @@ void ATankPlayerController::AimTowardsCrosshair()
 {
 	if (!GetControlledTank()) { return; }
 
-	// Get world location if linetrace through crosshair
-	// If it hits the landscape
-		// Tell controlled tank to aim at this point
+	FVector HitLocation;	// Out parameter
+	if (GetSightRayHitLocation(HitLocation))	// Has "side-effect", is going to line trace
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		// TODO Tell controller tank to aim at this point
+	}
+	
 
+}
+
+// Get world location if linetrace through crosshair, true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	HitLocation = FVector(1.0);
+	return true;
 }

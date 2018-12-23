75bf64399e7162d804495094a237f2561bce0a50
Unknown <graushf@gmail.com>
2018-12-23 12:08:05 +0100

    Changing code to reflect barrel elevation diagram

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -2,8 +2,8 @@
 
 #pragma once
 
-#include "Tank.h"
 #include "CoreMinimal.h"
+#include "Tank.h"
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerController.generated.h" // Must be the last include
 

03240b384461bb26fe7149e8cd6d61760a8612e7
Unknown <graushf@gmail.com>
2018-12-27 16:37:09 +0100

    Adding Programmed Sideways Correcting Force to TankTrack.cpp

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,6 +2,30 @@
 
 #include "public/TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Calculate the slippage speed
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+
+	// Work-out the required acceleration this frame to correct
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+
+	// Calculate and apply sideaways force (F = m * a)
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2;	// Two tracks
+	TankRoot->AddForce(CorrectionForce);
+
+	UE_LOG(LogTemp, Warning, TEXT("[D] - TankTrack is ticking"));
+}
+
+
 void UTankTrack::SetThrottle(float Throttle)
 {
 
